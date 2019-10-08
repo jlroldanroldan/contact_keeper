31efe9499379141117994f3c6b9004f8f217adcd
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2019-10-07 23:30:57 -0400

    Notes up to lect47. Contact Model & Get Contacts Route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth')
+const {check, validationResult } = require('express-validator')
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route       GET api/contacts
 // @desc        Register a users contacts
 // @access      Public 
-router.get('/', (req, res) => {
-    res.send('Get all contacts');
-});
-
+router.get(
+    '/', 
+    auth, 
+    async (req, res) => {
+        try {
+            const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1});
+            res.json(contacts);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
 // @route       POST api/contacts
 // @desc        Add new contact
 // @access      Private
 router.post('/', (req, res) => {
     res.send('Add contact');
 });
+
 
 // @route       PUT api/contacts/:id
 // @desc        Update contact
