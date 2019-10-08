bb8a114019a6af40ae5de77e71a4d213cde511a7
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2019-10-08 00:15:14 -0400

    notes up to lect 49. 49. Contact Update & Delete Routes. Only Update implemented

--- routes/contacts.js
+++ routes/contacts.js
@@ -63,13 +63,44 @@ router.post(
     }
 );
 
-
 // @route       PUT api/contacts/:id
 // @desc        Update contact
 // @access      Private
-router.put('/:id', (req, res) => {
-    res.send('Update contact');
-});
+router.put( 
+    '/:id',
+    auth, 
+    async (req, res) => {
+        const { name, email, phone, type } = req.body;
+
+        // Build contact object 
+        const contactFields = {};
+        if (name) contactFields.name = name;
+        if (email) contactFields.name = email;
+        if (phone) contactFields.name = phone;
+        if (type) contactFields.name = type;
+
+        try {
+            let contact = await Contact.findById(req.params.id);
+            
+            if(!contact) return res.status(404).json({ msg: 'Contact not found'});
+
+            // Make sure user owns contact
+            if(contact.user.toString()  !== req.user.id){
+                return res.status(401).json({ msg: 'Not Authorized'});
+            }
+
+            contact = await Contact.findByIdAndUpdate(req.params.id,
+                { $set: contactFields },
+                { new: true});
+
+            res.json(contact);
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
 
 // @route       DELETE api/contacts/:id
 // @desc        Delete contact
